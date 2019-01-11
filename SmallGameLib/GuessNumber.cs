5a1e820b4f5797a91b9c69ed2ef7df9af62f0a69
ttom921 <ttom921@hotmail.com>
2019-01-11 17:56:08 +0800

    加入WPF版

---
+++ SmallGameLib/GuessNumber.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallGameLib
+{
+    public class GuessNumber
+    {
+        public delegate void WriteLine(string s);
+        public WriteLine MessageLog;
+        // 所有的答案組合
+        int[] answerSet = new int[10 * 9 * 8 * 7];
+        int gessindex = -1;
+        /// <summary>
+        /// 產生所有的答案組合
+        /// </summary>
+        public void GenAnswerNum()
+        {
+            int index = 0;
+            for (int n1 = 0; n1 < 10; n1++)
+            {
+                for (int n2 = 0; n2 < 10; n2++)
+                {
+                    if (n2 == n1) continue;
+                    for (int n3 = 0; n3 < 10; n3++)
+                    {
+                        if (n3 == n2 || n3 == n1) continue;
+                        for (int n4 = 0; n4 < 10; n4++)
+                        {
+                            if (n4 == n1 || n4 == n2 || n4 == n3) continue;
+                            answerSet[index++] = n1 * 1000 + n2 * 100 + n3 * 10 + n4;
+                        }
+                    }
+                }
+            }
+            if(MessageLog!=null)
+            {
+                MessageLog("產生所有的答案組合");
+            }
+            gessindex = 0;
+        }
+        public string GetGestNumber()
+        {
+            return transform(answerSet[gessindex]);
+        }
+        
+        public void GetNextGuessNumber()
+        {
+            for (int i = 0; i < answerSet.Length; i++)
+            {
+                if (answerSet[i] != -1)
+                {
+                    gessindex = i;
+                    break;
+                }
+            }
+        }
+        public void reduce(int number, int a, int b)
+        {
+            for (int i = 0; i < answerSet.Length; i++)
+            {
+                if (answerSet[i] == -1) continue;
+                if (getA(number, answerSet[i]) != a || getB(number, answerSet[i]) != b)
+                    answerSet[i] = -1;
+            }
+            for (int i = 0; i < answerSet.Length; i++)
+            {
+                if (answerSet[i] == -1) continue;
+                MessageLog(transform(answerSet[i]) + " ");
+            }
+        }
+        //取得和此數字有相同位置
+        int getA(int n1, int n2)
+        {
+            int a = 0;
+            String str1 = transform(n1);
+            String str2 = transform(n2);
+            for (int i = 0; i < 4; i++)
+            {
+                if (str1[i] == str2[i])
+                    a++;
+            }
+            return a;
+        }
+        //取得此睥字有相同數字的
+        private int getB(int n1, int n2)
+        {
+            int b = 0;
+            String str1 = transform(n1);
+            String str2 = transform(n2);
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j) continue;
+                    if (str1[i] == str2[j])
+                        b++;
+                }
+            }
+            return b;
+        }
+        String transform(int n)
+        {
+            if (n < 1000)
+                return "0" + n;
+            else
+                return "" + n;
+        }
+    }
+}
---
+++ WpfGuestNumber/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using SmallGameLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGuestNumber
+{
+    public enum GameStet
+    {
+        None,
+        Start,
+        Guess,
+    }
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GameStet gameState = GameStet.None;
+        GuessNumber guessNumber = new GuessNumber();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            guessNumber.MessageLog = this.AddMessage;
+            AddMessage("start");
+            checkBtnState();
+        }
+        #region Message處理
+        delegate void UpdateWPFMsg(string s);
+        public void _UpDateString(string msg)
+        {
+            this.MsgTextBox.AppendText(DateTime.Now.ToString() + "  ");
+            this.MsgTextBox.AppendText(msg);
+            this.MsgTextBox.AppendText("\n");
+            this.MsgTextBox.ScrollToEnd();
+        }
+        public void AddMessage(String msg)
+        {
+            this.Dispatcher.Invoke(new UpdateWPFMsg(this._UpDateString), new object[] { msg });
+        }
+        #endregion //Message處理
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayerNumber.Text.Length < 4)
+            {
+                MessageBox.Show("請輸入4個不重覆的數字");
+                return;
+            }
+            gameState = GameStet.Start;
+            guessNumber.GenAnswerNum();
+            checkBtnState();
+            PCGuessNum.Text= guessNumber.GetGestNumber();
+
+        }
+        void checkBtnState()
+        {
+            if (gameState == GameStet.None)
+            {
+                BtnStart.IsEnabled = true;
+                BtnSendAB.IsEnabled = false;
+            }
+            if (gameState == GameStet.Start)
+            {
+                BtnStart.IsEnabled = false;
+                BtnSendAB.IsEnabled = true;
+            }
+        }
+
+        private void BtnSendAB_Click(object sender, RoutedEventArgs e)
+        {
+            int a = 0, b = 0;
+            String str = Tbnanb.Text;
+            if(str.Length != 4)
+            {
+                MessageBox.Show("輸入錯誤, 格式為 ?A?B ");
+                return;
+            }
+            a = str[0] - '0';
+            b = str[2] - '0';
+            int number = int.Parse(guessNumber.GetGestNumber());
+            if (a == 4)
+            {
+                string msg = $"答案是=>{number}";
+                MessageBox.Show(msg);
+                resetguess();
+                return;
+            }
+            guessNumber.reduce(number, a, b);
+            guessNumber.GetNextGuessNumber();
+            if(guessNumber.GetGestNumber()=="0-1")
+            {
+                MessageBox.Show("你騙人!! 根本沒這數字, ***!");
+                resetguess();
+                return;
+            }
+            PCGuessNum.Text = guessNumber.GetGestNumber();
+        }
+        private void resetguess()
+        {
+            gameState = GameStet.None;
+            guessNumber.GenAnswerNum();
+            PCGuessNum.Text = "";
+            checkBtnState();
+        }
+    }
+}
